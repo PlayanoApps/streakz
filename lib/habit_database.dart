69e9eaced0fbc54eaad2d44f8aaac166c1086499
Playano Apps <playano.info@gmail.com>
2025-09-18 18:06:50 +0200

    refactor

--- lib/habit_database.dart
+++ lib/habit_database.dart
@@ -21,31 +21,30 @@ import "package:path_provider/path_provider.dart";
       - if newObject uses auto-increment, a new record is added
 */
 
-
 class HabitDatabase extends ChangeNotifier {
   static late Isar isar;
 
-  /* -------------  S E T U P ------------- */  
+  /* -------------  S E T U P ------------- */
 
   // Initialize database
   static Future<void> initialize() async {
     final dir = await getApplicationDocumentsDirectory();
-    isar = await Isar.open(  
-      [HabitSchema, AppSettingsSchema],  // generated .g.dart classes
-      directory: dir.path
+    isar = await Isar.open(
+      [HabitSchema, AppSettingsSchema], // generated .g.dart classes
+      directory: dir.path,
     );
   }
 
   // Save first launch date (for heatmap)
   static Future<void> saveFirstLaunchSettings() async {
-
     // Check if any record exists
     final existingSettings = await isar.appSettings.get(0);
 
     if (existingSettings == null) {
       // Save first launch date
-      final settings = AppSettings()        // Initialize start date
-        ..firstLaunchDate = DateTime.now(); 
+      final settings =
+          AppSettings() // Initialize start date
+            ..firstLaunchDate = DateTime.now();
       await isar.writeTxn(() => isar.appSettings.put(settings));
 
       // Add default habits
@@ -54,25 +53,40 @@ class HabitDatabase extends ChangeNotifier {
   }
 
   static Future<void> loadDefaultHabits() async {
-    final lastDayOfMonth = DateTime(DateTime.now().year, DateTime.now().month + 1, 0);
+    final lastDayOfMonth = DateTime(
+      DateTime.now().year,
+      DateTime.now().month + 1,
+      0,
+    );
     // final lastDayOfMonth = DateTime.now();
 
     final defaultHabits = [
-      Habit()..name = "Reading"..order = 0    // Welcome! :)
+      Habit()
+        ..name = "Reading"
+        ..order =
+            0 // Welcome! :)
         ..completedDays = List.generate(
-          lastDayOfMonth.day,   // length
+          lastDayOfMonth.day, // length
           (i) => DateTime(DateTime.now().year, DateTime.now().month, i + 1),
         ),
-      Habit()..name = "Exercise"..order = 1  // Swipe left to edit or delete habits
+      Habit()
+        ..name = "Exercise"
+        ..order =
+            1 // Swipe left to edit or delete habits
         ..completedDays = List.generate(
-          (lastDayOfMonth.day ~/ 3),     // add every third day
-          (i) => DateTime(DateTime.now().year, DateTime.now().month, (i + 1) * 3),
+          (lastDayOfMonth.day ~/ 3), // add every third day
+          (i) =>
+              DateTime(DateTime.now().year, DateTime.now().month, (i + 1) * 3),
         ),
-      Habit()..name = "Journaling"..order = 2   // Long-press to reorder habits
-      ..completedDays = List.generate(
-          (lastDayOfMonth.day ~/ 5),     // add every third day
-          (i) => DateTime(DateTime.now().year, DateTime.now().month, (i + 1) * 5),
-        )
+      Habit()
+        ..name = "Journaling"
+        ..order =
+            2 // Long-press to reorder habits
+        ..completedDays = List.generate(
+          (lastDayOfMonth.day ~/ 5), // add every third day
+          (i) =>
+              DateTime(DateTime.now().year, DateTime.now().month, (i + 1) * 5),
+        ),
     ];
 
     // Save them to db
@@ -97,7 +111,8 @@ class HabitDatabase extends ChangeNotifier {
   /* Sync habits list with db and update UI */
   Future<void> updateHabitList() async {
     // when fetching habits, sort by order
-    List<Habit> habitsFromDb = await isar.habits.where().sortByOrder().findAll();
+    List<Habit> habitsFromDb =
+        await isar.habits.where().sortByOrder().findAll();
 
     // Update current habits list
     habitsList.clear();
@@ -108,47 +123,50 @@ class HabitDatabase extends ChangeNotifier {
     //🔥Sync to firestore
     // syncToFirestore();
     final user = FirebaseAuth.instance.currentUser;
-    if (user != null)
-      await FirestoreDatabase(isar).syncToFirestore(user);
+    if (user != null) await FirestoreDatabase(isar).syncToFirestore(user);
   }
 
   /* Add a new habit to db, update the list */
-  Future<void> addHabit(String habitName) async { 
+  Future<void> addHabit(String habitName) async {
     // Find the current highest order value
     final lastHabit = await isar.habits.where().sortByOrderDesc().findFirst();
     final newOrder = (lastHabit?.order ?? 0) + 1;
 
-    final newHabit = Habit()
-      ..name = habitName
-      ..order = newOrder;
+    final newHabit =
+        Habit()
+          ..name = habitName
+          ..order = newOrder;
 
-    // Isar creates a new habit with auto-incremented id 
+    // Isar creates a new habit with auto-incremented id
     // (if newHabit already had an id, isar would update it)
 
     await isar.writeTxn(() => isar.habits.put(newHabit));
 
-    updateHabitList();   
+    updateHabitList();
   }
 
   /* Update completedDays of a habit when checking it on and off */
-  Future<void> updateHabitCompletion(int id, bool isCompleted, DateTime passedDate) async {
-    final habit = await isar.habits.get(id);    // Find habit
+  Future<void> updateHabitCompletion(
+    int id,
+    bool isCompleted,
+    DateTime passedDate,
+  ) async {
+    final habit = await isar.habits.get(id); // Find habit
 
     if (habit != null) {
       // if habit is completed, add current date to completedDays list
       if (isCompleted && !habitCompleted(habit.completedDays, passedDate)) {
-        habit.completedDays.add(DateTime(
-          passedDate.year,
-          passedDate.month,
-          passedDate.day,
-        ));
+        habit.completedDays.add(
+          DateTime(passedDate.year, passedDate.month, passedDate.day),
+        );
       }
       // if not completed, remove current date from list
       else {
-        habit.completedDays.removeWhere((date) => 
-          date.year == passedDate.year &&
-          date.month == passedDate.month &&
-          date.day == passedDate.day
+        habit.completedDays.removeWhere(
+          (date) =>
+              date.year == passedDate.year &&
+              date.month == passedDate.month &&
+              date.day == passedDate.day,
         );
       }
       // save habit back to database
@@ -165,7 +183,7 @@ class HabitDatabase extends ChangeNotifier {
       habit.name = newName;
       await isar.writeTxn(() => isar.habits.put(habit));
     }
-    updateHabitList();   // Update habits list and UI
+    updateHabitList(); // Update habits list and UI
   }
 
   /* Delete habit */
@@ -174,7 +192,7 @@ class HabitDatabase extends ChangeNotifier {
       await isar.habits.delete(id);
     });
     updateHabitList();
-  }  
+  }
 
   /* Habit descripiton */
   Future<void> addDescription(int id, String newDescription) async {
@@ -204,7 +222,7 @@ class HabitDatabase extends ChangeNotifier {
         // Update order field of each habit to match the udpated habitsList
         habitsList[i].order = i;
         // Save each habit
-        await isar.habits.put(habitsList[i]); 
+        await isar.habits.put(habitsList[i]);
       }
     });
   }
