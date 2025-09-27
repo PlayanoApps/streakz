e4bc0ad90d9884ae7aff97e75d9ff4a77f1cf645
Playano Apps <playano.info@gmail.com>
2025-09-27 18:38:20 +0200

    new

--- lib/services/noti_service.dart
+++ lib/services/noti_service.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:habit_tracker/models/habit.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:timezone/standalone.dart';
 import 'package:timezone/timezone.dart' as tz;
 import "package:timezone/data/latest.dart" as tz;
 import "package:flutter_timezone/flutter_timezone.dart";
